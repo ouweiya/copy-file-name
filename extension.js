c6ec47e24d4e61ea85bc5fb57a589340c33eb83a
ouweiya <higogle1@gmail.com>
2022-05-29 13:46:44 +0800

    11

--- extension.js
+++ extension.js
@@ -1,36 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const activate = context => {
+  const qcl = vscode.commands.registerCommand('quick-console-log', () => {
+    const editor = vscode.window.activeTextEditor;
+    const selection = editor.selection;
+    const selectedText = editor.document.getText(selection);
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+    console.log('txt', selectedText);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quick-console-log" is now active!');
+    vscode.commands.executeCommand('editor.action.insertLineAfter').then(() => {
+      selectedText
+        ? editor.insertSnippet(new vscode.SnippetString(`console.log('${selectedText}', ${selectedText});`))
+        : editor.insertSnippet(new vscode.SnippetString(`console.log();`));
+    });
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quick-console-log.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  context.subscriptions.push(qcl);
+};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from quick console.log!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
+module.exports.activate = activate;
